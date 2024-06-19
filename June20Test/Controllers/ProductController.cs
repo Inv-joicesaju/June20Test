021e3aa01592ad5cb8ce8ff4cc2a28ca46742462
Inv-joicesaju <joice.saju@innovaturelabs.com>
2024-06-19 16:00:52 +0530

    feat:new controller

---
+++ June20Test/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace June20Test.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
